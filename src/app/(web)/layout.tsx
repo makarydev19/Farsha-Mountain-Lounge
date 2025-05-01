6721b2e01e737b0ac1a6a6187610e8f2740b0b25
makarydev19 <makarydev19@gmail.com>
2025-05-02 01:48:12 +0300

    fix error

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,22 +1,8 @@
-import { Kanit } from 'next/font/google'
-import localFont from 'next/font/local'
 import { NextAuthProvider } from '@/src/providers/AuthProvider'
 import Footer from '@/src/components/layout/Footer'
 import Header from '@/src/components/layout/Header'
 import Toast from '@/src/components/Toast/Toast'
 import './globals.css'
-
-const kanit = Kanit({
-  variable: '--font-kanit',
-  subsets: ['latin'],
-  weight: ['400', '500', '600']
-})
-
-const milky = localFont({
-  src: '../../../public/fonts/Milky-BF656b1e9e88c3a.woff',
-  variable: '--font-milky',
-  display: 'swap'
-})
 
 export default function RootLayout({
   children
@@ -25,9 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${kanit.variable} ${milky.variable} body-bg font-normal antialiased`}
-      >
+      <body className='body-bg font-normal antialiased'>
         <NextAuthProvider>
           <Toast />
           <main className='text-white'>
