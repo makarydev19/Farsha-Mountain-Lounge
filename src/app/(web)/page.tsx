101160cde1fdb1acdf8941efac8092e7d783eb07
makarydev19 <makarydev19@gmail.com>
2025-05-02 22:59:12 +0300

    update home page

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,13 +1,43 @@
-import { Hero, About, History, Guidelines, Services } from '@/src/sections/Home'
+'use client'
+
+import { Hero } from '@/src/sections/Home'
+import { useEffect, useState } from 'react'
+import dynamic from 'next/dynamic'
+
+const About = dynamic(() => import('@/src/sections/Home').then(m => m.About))
+const History = dynamic(() =>
+  import('@/src/sections/Home').then(m => m.History)
+)
+const Services = dynamic(() =>
+  import('@/src/sections/Home').then(m => m.Services)
+)
+const Guidelines = dynamic(() =>
+  import('@/src/sections/Home').then(m => m.Guidelines)
+)
+
+// Do the same for other sections...
 
 export default function Home() {
+  const [showRest, setShowRest] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowRest(true)
+    }, 2000) // Match Hero animation duration
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <>
       <Hero />
-      <About />
-      <History />
-      <Services />
-      <Guidelines />
+      {showRest && (
+        <>
+          <About />
+          <History />
+          <Services />
+          <Guidelines />
+        </>
+      )}
     </>
   )
 }
